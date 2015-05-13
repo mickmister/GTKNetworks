4e8c5dfe4af83b9831e1208d9f4dcdaf1f4a6386
Sean Kling <seanakl5@gmail.com>
2015-05-13 00:24:53 -0700

    added tcp to gui

--- scribble.c
+++ scribble.c
@@ -1,27 +1,83 @@
 #include <gtk/gtk.h>
 #include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <time.h>
 
 #define CHECK_SIZE 10
 #define SPACING 2
 #define BRUSH_SIZE_MIN 2
 #define BRUSH_SIZE_MAX 20
 
-static GtkWidget *window, *brushLabel, *entry, *drawing_area;
+int sockfd,n;
+struct sockaddr_in servaddr,cliaddr;
+char sendline[1000];
+char recvline[1000];
+clock_t start;
+
+GtkApplication *app;
+static GtkWidget *window, *brushLabel, *entry, *drawing_area, *image, *spinner;
+static GtkWidget *window2, *entry_msg, *button_send, *box_msg;
+GdkPixbuf * pb;
 static cairo_surface_t *surface = NULL;
 static brushSize = BRUSH_SIZE_MIN;
 gdouble brush_color[] = {0, 0, 0};
+int displayImage = 1, timer;
+unsigned int buffer[1000][3], bufferSize = 0;
 
-static void change_color(GtkWidget *widget, gpointer data){
-	char *color;
-	color = gtk_entry_get_text(GTK_ENTRY(entry));
-	sscanf(strtok(color, ",\0"), "%lf", &brush_color[0]);
-	sscanf(strtok(NULL, ",\0"), "%lf", &brush_color[1]);
-	sscanf(strtok(NULL, ",\0"), "%lf", &brush_color[2]);
+typedef struct{
+	char id;
+	unsigned int **buffer;
+} packet;
+
+static void activate_connect(GtkApplication* app, gpointer user_data);
+static void activate_drawing(GtkApplication* app, gpointer user_data);
+
+static void handle_buffer(unsigned int buffer[1000][3]){
+	int i;
+	sprintf(sendline, "x    y    size\n---------------\n");
+	sendto(sockfd,sendline,strlen(sendline),0,(struct sockaddr *)&servaddr,sizeof(servaddr));
+   n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+	for(i = 0; i < bufferSize; i++){
+		sprintf(sendline, "%3u  %3u  %u\n", buffer[i][0], buffer[i][1], buffer[i][2]);
+		sendto(sockfd,sendline,strlen(sendline),0,(struct sockaddr *)&servaddr,sizeof(servaddr));
+   	n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+	}	
+   recvline[n]=0;
+}
+
+static void handle_msg(char *msg){
+	//gtk_entry_get_text(GTK_ENTRY(entry_msg));
+	strcpy(sendline, msg);
+	sendto(sockfd,sendline,strlen(sendline),0,
+          (struct sockaddr *)&servaddr,sizeof(servaddr));
+   n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+   recvline[n]=0;
+   printf("Response: %s\n", recvline);
+}
+
+static void connect_to_server(GtkWidget *widget, gpointer data, GtkApplication *app){
+	gboolean active;
+	g_object_get(GTK_SPINNER(spinner), "active", &active, NULL);
+	g_print("Connecting to %s\n", gtk_entry_get_text(GTK_ENTRY(entry)));
+	if(active){
+		gtk_spinner_stop(GTK_SPINNER(spinner));
+	}else{
+		gtk_spinner_start(GTK_SPINNER(spinner));
+	}
+	if(connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == 0){
+		activate_drawing(app, data);
+		servaddr.sin_addr.s_addr=inet_addr(gtk_entry_get_text(GTK_ENTRY(entry)));
+		n=recvfrom(sockfd,recvline,10000,0,NULL,NULL);
+      recvline[n]=0;
+      fputs(recvline,stdout);
+      start = clock();
+	}
 }
 
 static void clear_surface(GtkWidget *widget){
 	cairo_t *cr;
-	GdkPixbuf * pb;
 	GtkAllocation alloc;
 	GdkWindow *da_window = gtk_widget_get_window(widget);
 	
@@ -30,7 +86,7 @@ static void clear_surface(GtkWidget *widget){
 	
 	if(gdk_window_is_viewable(da_window)){
 		pb = gdk_pixbuf_get_from_window(da_window, 0, 0, alloc.width, alloc.height);
-		gdk_pixbuf_save(pb, "file.bmp", "bmp", NULL, "quality", "100", NULL);
+		gdk_pixbuf_save(pb, "file.png", "png", NULL, NULL);
 	}
 	
 	cairo_set_source_rgb(cr, 1, 1, 1);
@@ -53,18 +109,24 @@ static gboolean scribble_configure_event(GtkWidget *widget, GdkEventConfigure *e
 																alloc.height);
 	
 	clear_surface(widget);
-	
 	return TRUE;
 }
 
 static gboolean draw_cb(GtkWidget *widget, cairo_t *cr, gpointer data){
+	if(displayImage){
+		surface = cairo_image_surface_create_from_png("file.png");
+		displayImage = 0;
+	}
 	cairo_set_source_surface(cr, surface, 0, 0);
 	cairo_paint(cr);
-	
+	//g_print("draw_cb\n");
 	return FALSE;
 }
 
-static void draw_brush(GtkWidget *widget, gdouble x, gdouble y){
+static void draw_brush(GtkWidget *widget, gdouble x, gdouble y, guint state){
+	char *msg;
+	clock_t diff;
+	int msec;
 	cairo_t *cr = NULL;
 	
 	cr = cairo_create(surface);
@@ -75,18 +137,51 @@ static void draw_brush(GtkWidget *widget, gdouble x, gdouble y){
 	cairo_destroy(cr);
 	
 	gtk_widget_queue_draw_area(widget, x-(brushSize/2), y-(brushSize/2), brushSize, brushSize);
+	
+	buffer[bufferSize][0] = (unsigned int)x;
+	buffer[bufferSize][1] = (unsigned int)y;
+	buffer[bufferSize][2] = (unsigned int)brushSize;
+	bufferSize++;
+	
+	// Time-based packet sending
+	/*diff = clock() - start;
+	msec = diff * 1000 / CLOCKS_PER_SEC;
+	if(msec > 1000){
+		if(!(state & GDK_BUTTON1_MASK)){
+			//sprintf(msg, "x: %d, y: %d, size: %d\n", (int)x, (int)y, brushSize);
+			//memcpy(&msg, &buffer, sizeof(buffer));
+			//handle_msg(msg);
+			handle_buffer(buffer);
+			memset(buffer, 0, sizeof(buffer[0][0])*1000*3);
+			bufferSize = 0;
+			//start = clock();
+		}
+	}*/	
+}
+
+static gint button_release_event(GtkWidget *widget, GdkEventButton *event, gpointer data){
+
+	if(event->button == GDK_BUTTON_PRIMARY){
+		handle_buffer(buffer);
+		memset(buffer, 0, sizeof(buffer[0][0])*1000*3);
+		bufferSize = 0;
+	}
+	return TRUE;
 }
 
 static gint scribble_button_press_event(GtkWidget *widget, GdkEventButton *event, gpointer data){
+	cairo_t *cr = NULL;
 	if(surface == NULL){
 		return FALSE;
 	}
-	
+	displayImage = 0;
 	if(event->button == GDK_BUTTON_PRIMARY){
-		draw_brush(widget, event->x, event->y);
+		draw_brush(widget, event->x, event->y, event->state);
 	}else if(event->button == GDK_BUTTON_SECONDARY){
 		clear_surface(widget);
 		gtk_widget_queue_draw(widget);
+	}else{
+		printf("middle-click\n");
 	}
 	
 	return TRUE;
@@ -98,7 +193,7 @@ static gboolean scribble_motion_notify_event(GtkWidget *widget, GdkEventMotion *
 	}
 	
 	if(event->state & GDK_BUTTON1_MASK){
-		draw_brush(widget, event->x, event->y);
+		draw_brush(widget, event->x, event->y, event->state);
 	}
 	
 	return TRUE;
@@ -125,19 +220,21 @@ static gboolean scribble_scroll_event(GtkWidget *widget, GdkEventScroll *event, 
 	return TRUE;
 }
 
-static void close_window(void){
-	if(surface){
-		cairo_surface_destroy(surface);
+static void close_window(GtkWidget *widget, gpointer data){
+	if(widget == window2){
+		if(surface){
+			cairo_surface_destroy(surface);
+		}
+		gtk_main_quit();
+	}else{
+		gtk_widget_destroy(window);
 	}
-	
-	gtk_main_quit();
 }
 
-static void activate(GtkApplication* app, gpointer user_data){
+static void activate_drawing(GtkApplication* app, gpointer user_data){
 
 	GtkWidget *button_color_change;
 	GtkWidget *button_box;
-	GtkWidget *image;
 	GtkWidget *frame, *vbox, *label;
 	
 	GdkPixbuf *pixbuf;
@@ -162,12 +259,6 @@ static void activate(GtkApplication* app, gpointer user_data){
 	brushLabel = gtk_label_new(NULL);
 	gtk_label_set_text(GTK_LABEL(brushLabel), "Brush Size: 2");
 	gtk_box_pack_start(GTK_BOX(vbox), brushLabel, FALSE, FALSE, 0);
-	entry = gtk_entry_new();
-	gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 0);
-	
-	button_color_change = gtk_button_new_with_label("Change Color");
-	g_signal_connect(button_color_change, "clicked", G_CALLBACK(change_color), NULL);
-	gtk_container_add(GTK_CONTAINER(vbox), button_color_change);
 	
 	frame = gtk_frame_new(NULL);
 	gtk_frame_set_shadow_type(GTK_FRAME(frame), GTK_SHADOW_IN);
@@ -180,25 +271,16 @@ static void activate(GtkApplication* app, gpointer user_data){
 	g_signal_connect(G_OBJECT(drawing_area), "configure-event", G_CALLBACK(scribble_configure_event), NULL);
 	g_signal_connect(G_OBJECT(drawing_area), "motion-notify-event", G_CALLBACK(scribble_motion_notify_event), NULL);
 	g_signal_connect(G_OBJECT(drawing_area), "button-press-event", G_CALLBACK(scribble_button_press_event), NULL);
+	g_signal_connect(G_OBJECT(drawing_area), "button-release-event", G_CALLBACK(button_release_event), NULL);
 	g_signal_connect(G_OBJECT(drawing_area), "scroll-event", G_CALLBACK(scribble_scroll_event), NULL);
 	gtk_widget_set_events(drawing_area, GDK_EXPOSURE_MASK
 								 | GDK_LEAVE_NOTIFY_MASK
 								 | GDK_BUTTON_PRESS_MASK
 								 | GDK_POINTER_MOTION_MASK
 								 | GDK_POINTER_MOTION_HINT_MASK
-								 | GDK_SMOOTH_SCROLL_MASK);
+								 | GDK_SMOOTH_SCROLL_MASK
+								 | GDK_BUTTON_RELEASE_MASK);
 								 
-	
-
-	/*
-	pixbuf = gdk_pixbuf_new_from_file("image.jpg", NULL);
-	width = gdk_pixbuf_get_width(pixbuf);
-	height = gdk_pixbuf_get_height(pixbuf);
-	pixbuf = gdk_pixbuf_scale_simple(pixbuf, width / 4, height / 4, GDK_INTERP_BILINEAR);
-	image = gtk_image_new_from_pixbuf(pixbuf);
-	gtk_container_add(GTK_CONTAINER(vbox), image);
-	//gdk_pixbuf_save(pixbuf, "file.bmp", "bmp", NULL, "quality", "100", NULL);
-	*/
 	if(!gtk_widget_get_visible(window)){
 		gtk_widget_show_all(window);
 	}else{
@@ -206,13 +288,63 @@ static void activate(GtkApplication* app, gpointer user_data){
 	}
 }
 
+static void activate_connect(GtkApplication* app, gpointer user_data){
+	GtkWidget *hbox_ip, *button_ip, *vbox;
+
+	window2 = gtk_application_window_new(app);
+	gtk_window_set_title(GTK_WINDOW(window2), "Connection Window");
+	g_signal_connect(G_OBJECT(window2), "destroy", G_CALLBACK(close_window), NULL);
+	gtk_window_set_default_size(GTK_WINDOW(window2), 200, 100);
+	gtk_container_set_border_width(GTK_CONTAINER(window2), 8);
+	
+	vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 8);
+	gtk_container_set_border_width(GTK_CONTAINER(vbox), 8);
+	gtk_container_add(GTK_CONTAINER(window2), vbox);
+	
+	hbox_ip = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 8);
+	gtk_container_set_border_width(GTK_CONTAINER(hbox_ip), 8);
+	gtk_container_add(GTK_CONTAINER(vbox), hbox_ip);
+	
+	entry = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(hbox_ip), entry, FALSE, FALSE, 0);
+	
+	button_ip = gtk_button_new_with_label("Connect");
+	g_signal_connect(button_ip, "clicked", G_CALLBACK(connect_to_server), app);
+	gtk_container_add(GTK_CONTAINER(hbox_ip), button_ip);
+	
+	spinner = gtk_spinner_new();
+	gtk_container_add(GTK_CONTAINER(hbox_ip), spinner);
+	
+	box_msg = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 8);
+	gtk_container_set_border_width(GTK_CONTAINER(box_msg), 8);
+	gtk_container_add(GTK_CONTAINER(vbox), box_msg);
+	
+	entry_msg = gtk_entry_new();
+	gtk_box_pack_start(GTK_BOX(box_msg), entry_msg, FALSE, FALSE, 0);
+	
+	button_send = gtk_button_new_with_label("Send");
+	g_signal_connect(button_send, "clicked", G_CALLBACK(handle_msg), NULL);
+	gtk_container_add(GTK_CONTAINER(box_msg), button_send);
+	
+	if(!gtk_widget_get_visible(window2)){
+		gtk_widget_show_all(window2);
+	}else{
+		gtk_widget_destroy(window2);
+	}
+}
+
 int main(int argc, char* argv[]){
 
-	GtkApplication *app;
 	int status;
 	
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_port=htons(8888);
+	
 	app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+	g_signal_connect(app, "activate", G_CALLBACK(activate_connect), NULL);
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
 	
